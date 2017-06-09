5b2bfda14e008f394b6e072a7e540ac701827703
Kai <mjj@absolute-performance.com>
2017-06-09 17:01:35 -0600

    Updated kitty.  Now with more cute! Added purr feature.  This required modifying the randomizer and making a cuteness function. Also meant I had to make a function that just draws kitty. A few other changes that were mostly cosmetic.

--- kitty.py
+++ kitty.py
@@ -39,6 +39,34 @@ class KittyBox():
         print('   \'\"\"\'   \'\"\"\'       ')
         return True
 
+    def draw_kitty_purr_L(self):
+        print "     .     .         "
+        print "     |\.-./| ~purr~  "
+        print "    /       \        "
+        print "   /  /\ /\  \  _    "
+        print "   \==  Y  ==/ ( \   "
+        print "    ;-._^_.-;   ) )  "
+        print "   /   \_/   \ /  /  "
+        print "   |   (_)   |/  /   "
+        print "  /|  |   |  |\ /    "
+        print " | '""'   |  | |     "
+        print "  \_  ;___|  |/      "
+        print('    \"\"    \'\"\"\'       ')
+
+    def draw_kitty_purr_R(self):
+        print "     .     .         "
+        print "     |\.-./|         "
+        print "    /       \ ~purr~ "
+        print "   /  /\ /\  \  _    "
+        print "   \==  Y  ==/ ( \   "
+        print "    ;-._^_.-;   ) )  "
+        print "   /   \_/   \ /  /  "
+        print "   |   (_)   |/  /   "
+        print "  /|  |   |  |\ /    "
+        print " | |  |   '""' |     "
+        print "  \|  |___;  _/      "
+        print('   \'\"\"\'    \"\"        ')
+
     def draw_kitty_L(self):
         print "     .     .         "
         print "     |\.-./|         "
@@ -55,8 +83,10 @@ class KittyBox():
         return True
 
     ## Load kitty art lines into a kitty lines list ##
-    def kitty_art(self, tail, meow):
+    def kitty_art(self, tail, meow, purr):
         mylog.debug('Loading kitty art into kitty list')
+        mylog.debug('Current purr state is %s' % purr)
+        mylog.debug('Current meow state is %s' % meow)
         kitty_lines = []
         line_1 = "      .     ."
         kitty_lines.append(line_1)
@@ -66,6 +96,10 @@ class KittyBox():
         else:
             line_2 = "      |\.-./|"
             line_3 = "     /       \ "
+        if purr == 'left':
+            line_2 = "      |\.-./| ~purr~"
+        if purr == 'right':
+            line_3 = "     /       \ ~purr~"
         kitty_lines.append(line_2)
         kitty_lines.append(line_3)
         if tail == "left":
@@ -95,17 +129,38 @@ class KittyBox():
         kitty_lines.append(line_8)
         line_9 = "   /|  |   |  |\ /"
         kitty_lines.append(line_9)
-        line_10 = "  | |  |   |  | |"
+        if purr == 'left':
+            line_10 = "  | \'\"\"\'   |  | |"
+            line_11 = "   \_  ;___|  |/"
+            line_12 = ('     \"\"    \'\"\"\'')
+        if purr == 'right':
+            line_10 = "  | |  |   \'\"\"\' |"
+            line_11 = "   \|  |___;  _/"
+            line_12 = ('    \'\"\"\'    \"\"')
+        if not purr:
+            line_10 = "  | |  |   |  | |"
+            line_11 = "   \|  |___|  |/"
+            line_12 = ('    \'\"\"\'   \'\"\"\'')
         kitty_lines.append(line_10)
-        line_11 = "   \|  |___|  |/"
         kitty_lines.append(line_11)
-        line_12 = ('    \'\"\"\'   \'\"\"\'')
         kitty_lines.append(line_12)
         mylog.debug('Kitty list is %s' % kitty_lines)
         return kitty_lines 
 
+    ## Draw the kitty ##
+    def draw_kitty(self, screen, kitty_lines):
+        mylog.debug('Drawing kitty')
+        current_line = 1
+        for line in kitty_lines:
+            screen.gotoXY(self.pStartX + 1, self.pStartY + current_line)
+            print line
+            mylog.debug('Drew line %s' % line)
+            current_line += 1
+        return True
+
     ## Draw the kitty box ##
-    def draw_box(self, screen, current_time):
+    def draw_box(self, screen):
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         mylog.debug('Drawing the kitty box')
         current_line = 0
         cell_height = 12
@@ -123,71 +178,79 @@ class KittyBox():
         mylog.debug('Kitty box drawn')
         return True
 
-    ## For random meows ##
-    def random_meow(self, kitty_time_left):
+    ## For random cute ##
+    def random_cute(self, kitty_time_left):
         mylog.debug("Time left for kitty is %s" % kitty_time_left)
-        if kitty_time_left > 900:
-            kitty_time_left = 900
-        if kitty_time_left < 240:
-            mew_time = randint(0,120)
+        cute_time = randint(120,240)
+        cuteness = self.cute_pick()
+        mylog.debug("Next random cute in %s seconds!" % cute_time)
+        return cute_time, cuteness
+    def cute_pick(self):
+        random_pick = randint(0,1000)
+        if random_pick % 2 == 0:
+            cuteness = 'purr'
         else:
-            mew_time = randint(120,kitty_time_left)
-        mylog.debug("Next random meow in %s seconds!" % mew_time)
-        return mew_time
+            cuteness = 'meow'
+        return cuteness
 
     ## Run the kitty script! ##
     def Execute(self):
-        if debug:
-            mylog.debug("Testing kitty art...")
-            ## Really I just put this in here to justify using the ascii art ##
-            ## funcitons I made when originally doing this script.           ##
-            self.draw_kitty_L()
-            self.draw_kitty_R()
-            time.sleep(5)
         mylog.debug('Kitty will hang out for %s seconds' % self.kitty_time)
         screen = terminal.get_terminal()
         screen.clear()
-        screen.set_title("Kitty!! =^..^=")
+        screen.set_title("=^..^=")
         tail = 'left'
+        purr_time = False
         meow = False
-        mew_time = self.random_meow(self.kitty_time)
-        mew_count = 1 
+        cute_time, cuteness = self.random_cute(self.kitty_time)
+        cute_count = 1 
+        purr = None
         while self.kitty_time > 0:
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             mylog.debug('Setting kitty screen parameters')
             mylog.debug('Kitty tail is %s' % tail)
-            mylog.debug('Drawing kitty box in Execute')
-            ## Are we gonna meow? ##
+            mylog.debug('Countdown to next cute time is %s seconds!' % (cute_time - cute_count))
+            ## Are we gonna do sumthin' cute? ##
             rlist, _, _ = select([sys.stdin], [], [], 1)
             if rlist: 
-                meow_check = sys.stdin.readline()
-                if meow_check:
-                    meow = True
-            if mew_count == mew_time:
-                mew_time = self.random_meow(self.kitty_time)
-                mew_count = 0 
+                cute_check = sys.stdin.readline()
+                if cute_check:
+                    cuteness = self.cute_pick() 
+            if cute_count == cute_time:
+                cute_time, cuteness = self.random_cute(self.kitty_time)
+                cute_count = 0 
+            if cuteness == 'meow':
                 meow = True
-            ## Draw the kitty box ##
-            self.draw_box(screen, current_time)
-            ## Gather kitty parameters ##
-            kitty = self.kitty_art(tail, meow)
+            if cuteness == 'purr':
+                purr_time = True
+            ## Gather kitty parameters and draw that kitty ##
+            if purr_time:
+                purr = 'left'
+                self.draw_box(screen)
+                kitty = self.kitty_art(tail, meow, purr)
+                self.draw_kitty(screen, kitty)
+                time.sleep(1)
+                purr = 'right'
+                self.draw_box(screen)
+                kitty = self.kitty_art(tail, meow, purr)
+                self.draw_kitty(screen, kitty)
+            else:
+                self.draw_box(screen)
+                kitty = self.kitty_art(tail, meow, purr)
+                self.draw_kitty(screen, kitty)
             if tail == 'left':
                 tail = 'right'
                 mylog.debug('Changed tail to %s' % tail)
             else:
                 tail = 'left'
                 mylog.debug('Changed tail to %s' % tail)
+            ## Reset and adjust counters for the next kitty iteration ##
             current_line = 1
-            ## Draw the kitty! ##
-            mylog.debug('Drawing kitty')
-            for line in kitty:
-                screen.gotoXY(self.pStartX + 1, self.pStartY + current_line)
-                print line
-                mylog.debug('Drew line %s' % line)
-                current_line += 1
             self.kitty_time -= 1
-            mew_count += 1
+            cute_count += 1
             meow = False
+            purr_time = False
+            purr = None
+            cuteness = None
         screen.clear()
         ##  Return screen to previous sizesize ##
         print("\x1b[8;%s;%st" % (self.rows, self.columns))
@@ -196,9 +259,8 @@ class KittyBox():
 ## Input the kitty parameters ##
 if __name__ == '__main__':
     import argparse
-    import os
     #Parse command line arguments
-    parser = argparse.ArgumentParser(add_help=True, description='Hang out with a virtual kitty!! Press <Enter> to make them meow!')
+    parser = argparse.ArgumentParser(add_help=True, description='Hang out with a virtual kitty!! Press <Enter> to make them do something cute!')
     parser.add_argument("-k", "--kitty_time", action="store", dest="kitty_time", default=None, help="how long the kitty will chill with you (in seconds)")
     parser.add_argument("-r", "--resize", action="store_true", dest="resize", default=False, help="if you wish to resize your window to fit only the kitty")
     parser.add_argument("--debug", action="store_true", dest="debug", default=False, help="display kitty debug messages")
